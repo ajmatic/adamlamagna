6fea75dab1eb8f0f54d283bbaa0756e7d31ceb06
Adam Lamagna <adamjames1920@gmail.com>
2014-03-09 15:35:26 -0400

    deleted ses folder, and looped questions in SSC

--- securecarcare/dist/js/script.js
+++ securecarcare/dist/js/script.js
@@ -1,5 +1,5 @@
 
-headerArray = ["Did you buy a used car that isn’t protected by a manufacturer’s warranty?", "Can you afford to pay for expensive on-going repairs to your vehicle?", "Enroll Now - Call us at <span class='number'>(800) 989-8086</span>"];
+headerArray = ["Did you buy a used car that isn’t protected by a manufacturer’s warranty?", "Can you afford to pay for expensive on-going repairs to your vehicle?", "Enroll Now - Call us at <span class='number'>(800) 989-8086</span>", "Do you plan to own your vehicle longer than the manufacturer's warranty will cover?"];
 
 headerIndex = 0;
 
@@ -18,7 +18,8 @@ function changeWords() {
 	$('#myHeader').html(headerArray[headerIndex]);
 	headerIndex++;
 	if (headerIndex >= headerArray.length) {
-		clearInterval(intervalHandle);
+		//clearInterval(intervalHandle);
+		headerIndex = 0;
 	}
 }
 
